b615028adb5de0c09ff060404bde0182253f8ace
Matias Lopez <matiaslopezd@gmail.com>
2020-12-16 18:24:57 -0300

    FIX:heroku port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || 3000;
 const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
